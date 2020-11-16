6cb5b21e243ab58ddf5501dd8bb3147ee9877d94
keval-s <46511090+keval-s@users.noreply.github.com>
2020-11-15 16:57:48 -0800

    Keeping application alive via CountDownLatch

--- src/main/java/com/kevshah/rbmq/example/RabbitmqExampleApplication.java
+++ src/main/java/com/kevshah/rbmq/example/RabbitmqExampleApplication.java
@@ -2,12 +2,24 @@ package com.kevshah.rbmq.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class RabbitmqExampleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RabbitmqExampleApplication.class, args);
+    @Bean
+    public CountDownLatch countDownLatch() {
+        return new CountDownLatch(1);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext context = SpringApplication.run(RabbitmqExampleApplication.class, args);
+        final CountDownLatch countDownLatch = context.getBean(CountDownLatch.class);
+        Runtime.getRuntime().addShutdownHook(new Thread(countDownLatch::countDown));
+        countDownLatch.await();
     }
 
 }
